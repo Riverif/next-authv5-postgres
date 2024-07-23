81256010388bd523a828cfa5adadc12ad184d492
Riverif <rifkialfiann@gmail.com>
2024-07-23 13:06:31 +0700

    4. login signup actions setup

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
 import { SignUpSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const signUp = async (values: z.infer<typeof SignUpSchema>) => {
   const validateFields = SignUpSchema.safeParse(values);
@@ -10,6 +13,23 @@ export const signUp = async (values: z.infer<typeof SignUpSchema>) => {
   if (!validateFields.success) {
     return { error: "Invalide fields!" };
   }
+
+  const { email, password, username } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
 
   return { success: "Email sent!" };
 };
